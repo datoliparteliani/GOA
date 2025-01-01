8671236b29a8db3bef7275d8f7ec4e60dfdc7230
unknown <lipod135@gmail.com>
2025-01-01 23:55:48 +0400

    first commit

---
+++ level 34/classwork/ex.py
@@ -0,0 +1,5 @@
+movies = ("Men in black", "Scott Pilgrim: vs the world", "Jumanji", "Good dellas", "Garfield", "Shrek")
+onemovie, *othermovies = movies
+print("first movie: ", onemovie)
+print("rest of the movies: ", othermovies)
+print(movies[0], movies[-1])
---
+++ level 34/homework/ex.py
@@ -0,0 +1,3 @@
+_tuple = (56, "👌", 24, "2", -90, "GOA")
+first, second, *rest = _tuple
+print(first, second)
